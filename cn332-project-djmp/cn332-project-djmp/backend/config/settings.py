a51dbff785493b1f67c46be777fb7925bfeee09d
Athichart Penwong <arm280747@gmail.com>
2026-04-20 12:55:05 +0700

    week6

--- cn332-project-djmp/cn332-project-djmp/backend/config/settings.py
+++ cn332-project-djmp/cn332-project-djmp/backend/config/settings.py
@@ -58,7 +58,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "cn332",
         "USER": "postgres",
-        "PASSWORD": "1234",
+        "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
