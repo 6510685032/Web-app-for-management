45f3e0be17158dcb96e1381d640b25c1c925e164
6610685031 <6610685031@student.tu.ac.th>
2026-05-17 08:02:03 +0700

    collectstatic

--- docker/django-rest/entrypoint.sh
+++ docker/django-rest/entrypoint.sh
@@ -17,5 +17,8 @@ python manage.py makemigrations
 echo "Applying database migrations..."
 python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Starting server..."
 exec python manage.py runserver 0.0.0.0:8000
